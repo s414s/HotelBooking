89649187572a5e75b689ff5152c454cad2ebfa09
Alberto Salas <alsalasher@gmail.com>
2024-04-01 20:54:12 +0200

    add seeders

--- Domain/Entities/Hotel.cs
+++ Domain/Entities/Hotel.cs
@@ -2,12 +2,11 @@ namespace Domain;
 public class Hotel : Entity
 {
     public string Name { get; set; } = String.Empty;
-    public Address? Address { get; set; }
+    public string Address { get; set; } = String.Empty;
     public IEnumerable<Room> Rooms { get; set; } = [];
-    public IEnumerable<User> Users { get; set; } = [];
 
     public Hotel() { }
-    public Hotel(string name, Address address)
+    public Hotel(string name, string address)
     {
         Id = new Guid();
         Name = name;
--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -30,6 +30,40 @@ serviceCollection.AddTransient<IMenuPrinter, MenuPrinter>();
 
 var serviceProvider = serviceCollection.BuildServiceProvider();
 
+// ==== Seeding repos ====
+var usersStorage = serviceProvider.GetService<IRepository<User>>();
+var hotelsStorage = serviceProvider.GetService<IRepository<Hotel>>();
+
+if(usersStorage?.GetAll().Count() == 0)
+{
+    usersStorage.Add(new User("alberto", "salas", "root"));
+    usersStorage.Add(new User("ana", "sanz", "root"));
+    usersStorage.SaveChanges();
+}
+
+if(hotelsStorage?.GetAll().Count() == 0)
+{
+    hotelsStorage.Add(new Hotel { Id = new Guid(), Name = "boston", Address = "boston address", Rooms =
+        [
+            new Room(1, RoomTypes.Single),
+            new Room(1, RoomTypes.Double),
+            new Room(1, RoomTypes.Single),
+            new Room(1, RoomTypes.Double),
+        ] }
+    );
+
+    hotelsStorage.Add(new Hotel { Id = new Guid(), Name = "melia", Address = "melia address", Rooms =
+        [
+            new Room(1, RoomTypes.Single),
+            new Room(1, RoomTypes.Double),
+            new Room(1, RoomTypes.Single),
+            new Room(1, RoomTypes.Double),
+        ] }
+    );
+
+    hotelsStorage.SaveChanges();
+}
+
 // Resolve an instance of IConsoleLogger
 var menuPrinter = serviceProvider.GetService<IMenuPrinter>();
 
